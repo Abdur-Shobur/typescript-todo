35701d7bcda0809c168e0b1af64af25dfe863f58
abdur-shobur <abdurshobur.developer@gmail.com>
2023-05-02 00:57:00 +0600

    init file

---
+++ src/domInit.ts
@@ -0,0 +1,11 @@
+export const formHandler: HTMLElement | null =
+  document.getElementById('formHandler');
+
+export let inputField =
+  document.querySelector<HTMLInputElement>('#input-field')!;
+
+export const form = document.getElementById(
+  'formHandler',
+) as HTMLFormElement | null;
+
+export const taskBox = document.getElementById('task-box') as HTMLElement;
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,78 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { inputField, form, taskBox } from './domInit';
 
-import confetti from 'canvas-confetti';
+type todoItems = {
+  id: number;
+  name: string;
+  isComplete: boolean;
+};
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const toDo: todoItems[] = [];
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let input = inputField?.value;
+  if (input === '' || input === undefined || input === null) return;
+  const data: todoItems = {
+    id: toDo.length,
+    isComplete: false,
+    name: input,
+  };
+  toDo.push(data);
+  showData(data);
+  inputField.value = '';
+});
+
+function showData(todo: todoItems) {
+  taskBox.innerHTML = toDo
+    .map(
+      (todo) => `
+      <li class="task">
+        <label  onclick="handleChecked(${todo.id})" for="task-${todo.id}">
+          <input type="checkbox" id="task-${todo.id}" ${
+        todo.isComplete ? 'checked' : ''
+      } />
+          <p class="${todo.isComplete ? 'completed' : ''}">${todo.name}</p>
+        </label>
+        <div class="settings">
+          <i class="uil uil-ellipsis-h">...</i>
+          <ul class="task-menu">
+            <li><i class="uil uil-pen"></i>Edit</li>
+            <li><i class="uil uil-trash"></i>Delete</li>
+          </ul>
+        </div>
+      </li>
+    `,
+    )
+    .join('');
+}
+function handleChecked(id: number) {
+  const todo = toDo.find((e) => e.id === id);
+  if (todo) {
+    todo.isComplete = !todo.isComplete;
+    showData(todo);
+  }
+}
+// function showData(todo: todoItems) {
+//   taskBox.innerHTML = toDo
+//     .map(
+//       (todo) => `
+//     <li class="task">
+//       <label onclick="handelChecked(${todo.id})" for="${todo.name + todo.id}">
+//         <input type="checkbox" id="${todo.name + todo.id}" ${
+//         todo.isComplete ? 'checked' : ''
+//       } />
+//         <p class="${todo.isComplete ? 'completed' : ''}">${
+//         todo.name + todo.id
+//       }</p>
+//       </label>
+//       <div class="settings">
+//         <i  class="uil uil-ellipsis-h">...</i>
+//         <ul class="task-menu">
+//           <li><i class="uil uil-pen"></i>Edit</li>
+//           <li><i class="uil uil-trash"></i>Delete</li>
+//         </ul>
+//       </div>
+//     </li>
+//   `,
+//     )
+//     .join('');
+// }
